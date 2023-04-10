10acc6ff7173b8bc4247585b6b9eedd973c73f87
ducanh2712 <phamducanh2712@gmail.com>
2023-04-10 11:58:11 +0900

    New HSV scale tool

---
+++ Find_HSV/hsv_scale.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+
+# A required callback method that goes into the trackbar function.
+def nothing(x):
+    pass
+
+# Load the image from file
+img = cv2.imread('Crop_Dataset/images/IMG_3897_JPG_jpg.rf.b1866ceef68cb91e551a84f732795207.jpg')
+
+# Resize the image to fit the screen if needed
+img = cv2.resize(img, (1280, 720))
+
+# Create a window named trackbars.
+cv2.namedWindow("Trackbars")
+
+# Now create 6 trackbars that will control the lower and upper range of 
+# H,S and V channels. The Arguments are like this: Name of trackbar, 
+# window name, range,callback function. For Hue the range is 0-179 and
+# for S,V its 0-255.
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+# Convert the BGR image to HSV image.
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+while True:
+    
+    # Get the new values of the trackbar in real time as the user changes 
+    # them
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+ 
+    # Set the lower and upper HSV range according to the value selected
+    # by the trackbar
+    lower_range = np.array([l_h, l_s, l_v])
+    upper_range = np.array([u_h, u_s, u_v])
+    
+    # Filter the image and get the binary mask, where white represents 
+    # your target color
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+ 
+    # You can also visualize the real part of the target color (Optional)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    
+    # Converting the binary mask to 3 channel image, this is just so 
+    # we can stack it with the others
+    mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    
+    # stack the mask, orginal frame and the filtered result
+    stacked = np.hstack((mask_3,img,res))
+    
+    # Show this stacked frame at 40% of the size.
+    cv2.imshow('Trackbars',cv2.resize(stacked,None,fx=0.4,fy=0.4))
+    
+    # If the user presses ESC then exit the program
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+    # If the user presses `s` then print this array.
+    if key == ord('s'):
+        
+        thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
+        print(thearray)
+        
+        # Also save this array as hsv_value.npy
+        np.save('hsv_value',thearray)
+        break
+    
+# Destroy the windows.    
+cv2.destroyAllWindows()
